4cf031c8ded6b273ac7f4ca1faca6bffd6b0951d
Felix <90440279+Kubenew@users.noreply.github.com>
2021-11-11 21:18:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 //-----------------------------------------------------------------------------
 // NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
-// Copyleft (c) by Denis Astahov
+// Copyleft (c) 
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.1</b>";
+  const message="<font color='blue'>СloudFunction of GKE</font><br><b>App Version 1.1</b>";
   res.status(200).send(message);
 };
