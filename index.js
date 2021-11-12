12c8d3db14029497e45b06d323c3d3e99589a05c
Felix <90440279+Kubenew@users.noreply.github.com>
2021-11-12 19:25:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction CD/CI Serverless</font><br><b>App Version 1.1</b>";
+  const message="<font color='blue'>СloudFunction CD/CI Serverless</font><br><b>App Version 1.15</b>";
   res.status(200).send(message);
 };
